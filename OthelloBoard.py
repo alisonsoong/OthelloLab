2828a52ef795b2ce1f2daba738a34692731b7714
derikliu <102180191+derikliu@users.noreply.github.com>
2022-03-28 09:30:57 -0700

    Update OthelloBoard.py

--- OthelloBoard.py
+++ OthelloBoard.py
@@ -1,2 +1,12 @@
 #blub
 #change!
+class Board:
+    def __init__():
+        board = [
+                [], [], [], [], [], [], [], []
+                ]
+        for index in board:
+            for i in range(8):
+                index.append(None)
+
+        
